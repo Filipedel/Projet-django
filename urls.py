3d62effebae5e1f4344a4d04fa2eb905e04b9aa0
FILIPE <92913654+Filipedel@users.noreply.github.com>
2021-11-27 14:25:41 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,26 +1,21 @@
-"""lesrestosducrous URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
+from . import views
+
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('', include('polls.urls')),
-    path('polls/',include('django.contrib.auth.urls')),
-
-
+    path('inscription', views.index3, name="index3"),
+    path('connexion',views.index2,name="index2"),
+    path('', views.index, name="index"),
+    path('Bienvenue',views.Bienvenue,name="bv"),
+    path('logout',views.ciao,name="ciao"),
+    path('beneficiaire',views.benefi,name="beneto"),
+    path('produits',views.produitadd,name="prod"),
+    path('categorie',views.categorieadd,name="cate"),
+    path('categorie/delete/<int:id>', views.delete, name='delete'),
+    path('produits/delete/<int:id>', views.delete2, name='delete2'),
+    path('beneficiaire/SUCCESS/<int:id>',views.seebenefi,name='see'),
+    path('beneficiaire/SUCCESS/valide/<int:id>',views.valbenef,name='success'),
+    path('beneficiaire/SUCCESS/delete/<int:id>', views.delbenef, name='supprime'),
+    path('validation', views.valide, name='succeed'),
 ]
